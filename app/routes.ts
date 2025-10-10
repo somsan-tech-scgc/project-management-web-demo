788ff74a56c2d06534736737058b30807635513a
realroy <lilrubyboi@gmail.com>
2025-10-10 18:32:18 +0700

    refactor: route

--- app/routes.ts
+++ app/routes.ts
@@ -1,13 +1,17 @@
+// https://reactrouter.com/start/framework/routing
 import {
   type RouteConfig,
   index,
   route,
   layout,
+  prefix,
 } from "@react-router/dev/routes";
 
 export default [
   layout("routes/layout.tsx", [
     index("routes/dashboard/dashboard.tsx"),
-    route("/projects/:id", "routes/project-detail/project-detail.tsx"),
+    ...prefix("/projects", [
+      route(":id", "routes/project-detail/project-detail.tsx"),
+    ]),
   ]),
 ] satisfies RouteConfig;
